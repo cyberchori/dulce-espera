cde93f5618cf567527ffbcb21631a09c526e7c92
Fernando Daniel Albertengo <fernando.albertengo@gmail.com>
2024-06-03 01:21:59 +0000

    Quitados componentes generados con IA incorrectamente y reiniciado el ejericicio con metas claras

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+/**
+ * No se pueden repetir nombres (de lo que sea) dentro del mismo scope
+ * (y no deberias repetir JAMAS un nombre de componente)
+ *
+ * Scope: Refiere al entorno de ejecucion actual, o a un bloque de codigo especifico, demarcado por los siguientes limites:
+ * - Global (toda tu aplicacion, por ejemplo, variables de entorno)
+ * - Archivo (todo lo que contenga un archivo especifico)
+ * - Local
+ *    - Dentro de un componente
+ *    - Dentro de una funcion
+ *
+ */
+
+/**
+ * 1) Dibujar la pagina EN UN UNICO COMPONENTE (App.jsx) cajitas de colores representando cada parte de la pagina
+ *   - rojo: navbar
+ *   - amarillo: carousel
+ *   - celeste: filtro
+ *   - verde: catalogo
+ * 2) Extrae cada componente en su propio componente, creando su archivo en la carpeta components, exportandolos e importandolos aqui
+ */
+function App() {
+  return <></>;
+}
+
+export default App;
