8173e0d8873cfca4ffabd4da88f4f01fbc4dcc09
alejandro966 <152237723+alejandro966@users.noreply.github.com>
2024-06-05 00:32:43 +0000

    Aniadi botonen  y estilos a Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,20 @@ import "./App.css";
 function App() {
   const Navbar = () => {
     return (
-      <div style={{ backgroundColor: 'red', padding: 'flex', borderRadius: '2px', display: 'flex', height: '2.5rem' }}>navbar</div>
+      <div style={{ backgroundColor: 'red', padding: 'flex', borderRadius: '1rem', display: 'flex', height: '2.5rem',alignItems: 'center', justifyContent: 'space-between',  fontSize: '24px', fontWeight: 'bold', color: '#ccc', marginRight: 'auto', fontFamily: 'Arial, sans-serif', textShadow: '1px 1px 2px rgba(0, 0, 0, 0.2)', letterSpacing: '0.02em', transition: 'all 0.3s ease,'}}>DULCE ESPERA
+      <input
+          style={{ backgroundColor: 'white', border: 'none', borderRadius: '2rem', padding: '0.5rem 1rem',display:'flex' }}
+          type="text"
+          placeholder="Buscar"
+        />
+       <div style={{ display: 'flex', gap: '1rem' }}>
+        <button style={{ backgroundColor: 'white', border: 'none', borderRadius: '2rem', padding: '0.3rem 0.2rem' }}>button 1</button>
+        <button style={{ backgroundColor: 'white', border: 'none', borderRadius: '2rem', padding: '0.3rem 0.2rem' }}>button 2</button>
+        <button style={{ backgroundColor: 'white', border: 'none', borderRadius: '2rem', padding: '0.3rem 0.2rem' }}>USUARIO</button>
+        <button style={{ backgroundColor: 'white', border: 'none', borderRadius: '2rem', padding: '0.3rem 0.2rem' }}>Compras</button>
+      </div>
+      </div>
+      
     );
   };
 
